77dfe8875ccbcd9dedaf9ba48fd56dac6eb2242d
“p-szymo” <joshua.szymanowski@gmail.com>
2024-05-10 22:20:11 -0400

    try caching

--- live_scrape.py
+++ live_scrape.py
@@ -97,54 +97,6 @@ def current_team_score(results, team):
     return total_score, not_playing, dnf, team_results
 
 
-def score_it(league, event_number, to_print=False):
-    results = live_scorer(event_number)
-    scores = {}
-    team_results = {}
-
-    for team in league:
-        owner = team['owner']
-
-        score, not_playing, dnf, team_result = current_team_score(results, team)
-
-        if not_playing:
-            addenda = ' # not playing: ' + ' | '.join(not_playing)
-        else:
-            addenda = ''
-
-        if dnf:
-            addenda += ' # did not finish: ' + ' | '.join(dnf)
-
-        scores[owner] = {'score': score, 'addenda': addenda}
-        team_results[owner] = team_result
-
-    sorted_scores = {k: v for k, v in sorted(scores.items(), key=lambda item: item[1]['score'])}
-
-    printout = []
-
-    for owner, score in sorted_scores.items():
-        line = f"{owner}: {str(score['score'])}{score['addenda']}"
-        printout.append(line)
-
-    league_print_string = '\n'.join(printout)
-    if to_print: print(league_print_string + '\n')
-
-    full_printout = []
-    for owner, team_result in team_results.items():
-        sorted_results = {k: v for k, v in sorted(team_result.items(), key=lambda item: item[1])}
-        full_printout.append('\n' + owner)
-        if to_print: print(owner)
-        for player, score in sorted_results.items():
-            player_score = f'{player}: {score}'
-            full_printout.append(player_score)
-            if to_print: print(player_score)
-
-    teams_print_string = '\n'.join(full_printout)
-    if to_print: print(teams_print_string)
-
-    return league_print_string, teams_print_string
-
-
 ###########
 # TESTING #
 ###########
